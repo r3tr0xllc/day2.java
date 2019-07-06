da1a57dbd328219e931e8f286dc64ba3176a96e8
r3tr0xllc <49368156+r3tr0xllc@users.noreply.github.com>
2019-07-07 00:47:20 +0300

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,11 @@
+//serinin ikinci videosu dört işlem 
+class daytwo{
+public static void main(String[] args){
+int a=10;
+int b=20;
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+}
+}
